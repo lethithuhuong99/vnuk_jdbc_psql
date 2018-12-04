d64573575c8870b448cf54062771f006185f06a5
Modern Coding <cormorant.programming@gmail.com>
2018-12-04 07:55:59 +0700

    Environment variables

--- src/vn/edu/vnuk/jdbc/psql/ConnectionFactory.java
+++ src/vn/edu/vnuk/jdbc/psql/ConnectionFactory.java
@@ -8,11 +8,11 @@ public class ConnectionFactory {
 	public Connection getConnection() {
 		
 		try {
-		return DriverManager.getConnection(
-			"jdbc:postgresql://localhost/vnuk_personnel",
-			"ThuHuong",
-			"Toi La Huong"
-		);
+			return DriverManager.getConnection(
+				"jdbc:postgresql://localhost/vnuk_personnel", 
+				System.getenv("db_user"), 
+				System.getenv("db_password")
+			);
 		}
 		catch (SQLException e) {
 			throw new RuntimeException(e);
